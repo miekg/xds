9c5605f3d385f4b116c9e59f8ee0b4cab8a00503
Miek Gieben <miek@miek.nl>
2020-01-30 12:00:47 +0000

    cleanups and better
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- health.go
+++ health.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	corepb "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
-	healthpb "github.com/envoyproxy/go-control-plane/envoy/service/health/v3"
 	"github.com/urfave/cli/v2"
 )
 
@@ -21,39 +20,37 @@ func health(c *cli.Context) error {
 }
 
 // health sets the health for all endpoints in the cluster.
-func healthStatus(c *cli.Context, status string) error {
-	// cluster [endpoint] health
-	args := c.Args().Slice()
-	if len(args) < 2 || len(args) > 3 {
-		return ErrArg(args)
-	}
-	// list cluster and then ....
-	cluster := args[0]
-	endpoint := ""
-	health := ""
-	if len(args) == 2 {
-		health = args[1]
-	}
-	if len(args) == 3 {
-		endpoint = args[1]
-		health = args[2]
-	}
+func healthStatus(c *cli.Context, health string) error {
 	if healthNameToValue(health) == -1 {
 		return fmt.Errorf("unknown type of health: %s", health)
 	}
 
-	cl, err := New(c)
-	if err != nil {
-		return err
+	args := c.Args().Slice()
+	if len(args) < 2 || len(args) > 3 {
+		return ErrArg(args)
 	}
-	defer cl.Stop()
 
-	if cl.dry {
-		return nil
+	cluster := args[0]
+	endpoint := ""
+	if len(args) == 3 {
+		endpoint = args[1]
 	}
-	hr := &healthpb.HealthCheckRequestOrEndpointHealthResponse{}
-	hds := healthpb.NewHealthDiscoveryServiceClient(cl.cc)
-	_, err = hds.FetchHealthCheck(c.Context, hr)
+
+	/*
+
+		cl, err := New(c)
+		if err != nil {
+			return err
+		}
+		defer cl.Stop()
+
+		if cl.dry {
+			return nil
+		}
+		hr := &healthpb.HealthCheckRequestOrEndpointHealthResponse{}
+		hds := healthpb.NewHealthDiscoveryServiceClient(cl.cc)
+		_, err = hds.FetchHealthCheck(c.Context, hr)
+	*/
 	return err
 }
 

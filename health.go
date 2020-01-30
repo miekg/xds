ad28b06a76a3a4018bfc03244b831f0204ec2dc9
Miek Gieben <miek@miek.nl>
2020-01-30 12:04:33 +0000

    compile
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- health.go
+++ health.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	corepb "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	healthpb "github.com/envoyproxy/go-control-plane/envoy/service/health/v3"
 	"github.com/urfave/cli/v2"
 )
 
@@ -30,27 +31,25 @@ func healthStatus(c *cli.Context, health string) error {
 		return ErrArg(args)
 	}
 
-	cluster := args[0]
+	//	cluster := args[0]
 	endpoint := ""
 	if len(args) == 3 {
 		endpoint = args[1]
 	}
+	endpoint = endpoint
 
-	/*
+	cl, err := New(c)
+	if err != nil {
+		return err
+	}
+	defer cl.Stop()
 
-		cl, err := New(c)
-		if err != nil {
-			return err
-		}
-		defer cl.Stop()
-
-		if cl.dry {
-			return nil
-		}
-		hr := &healthpb.HealthCheckRequestOrEndpointHealthResponse{}
-		hds := healthpb.NewHealthDiscoveryServiceClient(cl.cc)
-		_, err = hds.FetchHealthCheck(c.Context, hr)
-	*/
+	if cl.dry {
+		return nil
+	}
+	hr := &healthpb.HealthCheckRequestOrEndpointHealthResponse{}
+	hds := healthpb.NewHealthDiscoveryServiceClient(cl.cc)
+	_, err = hds.FetchHealthCheck(c.Context, hr)
 	return err
 }
 

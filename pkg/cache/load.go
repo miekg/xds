65590708f078e1bd3444771a0bc0c524481e3539
Miek Gieben <miek@miek.nl>
2020-07-08 09:31:41 +0200

    Small cleanups in the lrs code
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- pkg/cache/load.go
+++ pkg/cache/load.go
@@ -3,13 +3,22 @@ package cache
 import (
 	loadpb "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v2"
 	"github.com/golang/protobuf/ptypes/duration"
+	"github.com/miekg/xds/pkg/log"
 )
 
 // SetLoad sets the load for clusters and or endpoints. Adjust weights here as well??
 func (c *Cluster) SetLoad(req *loadpb.LoadStatsRequest) (*loadpb.LoadStatsResponse, error) {
+	// we should check if we have the cluster, and then do something with the load.
+	// depending on LBPolicy we do different things with it?? Or just adjust the weights.
+	nodeID := req.GetNode().GetId()
+	for _, clusterStats := range req.ClusterStats {
+		if len(clusterStats.UpstreamLocalityStats) > 0 {
+			log.Debug("Got stats from cluster `%s` node `%s` - %s", req.Node.Cluster, nodeID, clusterStats)
+		}
+	}
 	return &loadpb.LoadStatsResponse{
 		Clusters:                  []string{req.GetNode().GetId()},
-		LoadReportingInterval:     &duration.Duration{Seconds: 10},
+		LoadReportingInterval:     &duration.Duration{Seconds: 10}, // was 2 seconds originally
 		ReportEndpointGranularity: true,
 	}, nil
 }
--- pkg/server/load.go
+++ pkg/server/load.go
@@ -6,14 +6,10 @@ import (
 	"sync/atomic"
 
 	loadpb "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v2"
-	"github.com/miekg/xds/pkg/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
-// This is how often Envoy will send the load report
-const StatsFrequencyInSeconds = 2
 
 type loadStream interface {
 	grpc.ServerStream
@@ -39,12 +35,6 @@ func (s *server2) loadProcess(stream loadStream, reqCh <-chan *loadpb.LoadStatsR
 			if req == nil {
 				return status.Errorf(codes.Unavailable, "empty request")
 			}
-			nodeID := req.GetNode().GetId()
-			for _, clusterStats := range req.ClusterStats {
-				if len(clusterStats.UpstreamLocalityStats) > 0 {
-					log.Debug("Got stats from cluster `%s` node `%s` - %s", req.Node.Cluster, nodeID, clusterStats)
-				}
-			}
 
 			resp, err := s.s.cache.SetLoad(req)
 			if err != nil {
@@ -61,7 +51,6 @@ func (s *server2) loadHandler(stream loadStream) error {
 	go func() {
 		for {
 			req, err := stream.Recv()
-			log.Debug("loadHandler called")
 			if atomic.LoadInt32(&reqStop) != 0 {
 				return
 			}
@@ -81,39 +70,3 @@ func (s *server2) loadHandler(stream loadStream) error {
 func (s *server2) StreamLoadStats(stream loadpb.LoadReportingService_StreamLoadStatsServer) error {
 	return s.loadHandler(stream)
 }
-
-/*
-func (s *server) HandleRequest(stream loadpb.LoadReportingService_StreamLoadStatsServer, request *loadpb.LoadStatsRequest) {
-	nodeID := request.GetNode().GetId()
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	// Check whether any Node has already connected or not.
-	// If not, add the NodeID to nodesConnected and enable Load Report with given frequency
-	// If yes, log stats
-	if _, exist := s.nodesConnected[nodeID]; !exist {
-		// Add NodeID to the nodesConnected
-		log.Printf("Adding new new node to cache `%s`", nodeID)
-		s.nodesConnected[nodeID] = true
-
-		// Initialize Load Reporting
-		err := stream.Send(&loadpb.LoadStatsResponse{
-			Clusters:                  []string{"local_service"},
-			LoadReportingInterval:     &duration.Duration{Seconds: StatsFrequencyInSeconds},
-			ReportEndpointGranularity: true,
-		})
-		if err != nil {
-			log.Panicf("Unable to send response to node %s due to err: %s", nodeID, err)
-		}
-		return
-	}
-
-	// After Load Report is enabled, log the Load Report stats received
-	for _, clusterStats := range request.ClusterStats {
-		if len(clusterStats.UpstreamLocalityStats) > 0 {
-			log.Printf("Got stats from cluster `%s` node `%s` - %s", request.Node.Cluster, request.Node.Id, clusterStats)
-		}
-	}
-}
-*/
